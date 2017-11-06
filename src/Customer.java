99310d6125b3ac8035610bbd324c20632de5b3b9
sherifabdlnaby <sherifabdlnaby@gmail.com>
2017-11-06 15:10:09 +0200

    Code Cleaning

--- src/Customer.java
+++ src/Customer.java
@@ -5,84 +5,75 @@ public class Customer extends Thread {
     public String Name;
     public Table Table;
     public Restaurant Restaurant;
+    private Random rand = new Random();
     public Customer(String name, Restaurant restaurant) {
         Name = name;
         Restaurant = restaurant;
     }
-    private static int randomTime(){
-        Random rand = new Random();
+
+    private void waitRandomTime(){
         int x = rand.nextInt(2001);
-        return 500 + x;
+        try {
+            sleep(500 + x);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
     public void run(){
         //Enter The Restaurant
         Restaurant.enterRestaurant(this);
+        System.out.println(Name + " Entered the restaurant.");
 
         //Try to Occupy Table
-        if (Restaurant.occupyTable(this)) {
-            System.out.println(Name + " Entered the restaurant and sat-down on Table #" + Table.ID);
-        }
-        else {
-            //Wait Turn
-            System.out.println(Name + " Entered the restaurant and waiting");
-            try {
-                synchronized (this){
-                    //Will be notified when someone Leaves a Table.
-                    do
-                        wait();
-                    while(!Restaurant.occupyTable(this));
-                    // While Loop for Sanity Check(however it shouldn't be notified unless there is a free table)
+        try {
+            synchronized (this){
+
+                while(!Restaurant.occupyTable(this)) {
+                    System.out.println(Name + " is waiting");
+                    wait();     //Will be notified when someone Leaves a Table.
                 }
-                System.out.println(Name + " sat-down on Table #" + Table.ID);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                // While Loop for Sanity Checking and Synchronization
+                // (however it shouldn't be notified unless there is a free table)
             }
+            System.out.println(Name + " sat-down on Table #" + Table.ID);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        //UI Elements Manipulation Variables
         Dashboard.ChessButton Button;
         int TableID = Table.ID;
         Button = (Dashboard.ChessButton) Restaurant.Dashboard.getComponent(TableID);
+
+        //GUI-Update
         Button.setBackground(Color.BLUE);
         Button.setText(TableID + " : " + Name + " sat-down");
 
         //Order Food
-        try {
-            sleep(randomTime());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        waitRandomTime();
+        //GUI-Update
         Button.setText(TableID + " : " + Name + " Ordered Food.");
         Button.setBackground(Color.yellow);
 
         //Eat Food
-        try {
-            sleep(randomTime());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        waitRandomTime();
+        //GUI-Update
         Button.setText(TableID + " : " + Name + " Eating.");
         Button.setBackground(Color.green);
 
         //Leaving
-        try {
-            sleep(randomTime());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        waitRandomTime();
+        //GUI-Update
         Button.setText(TableID + " : " + Name + " Leaving.");
         Button.setBackground(Color.red);
 
         //Leave
-        try {
-            sleep(randomTime());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        waitRandomTime();
         Restaurant.leaveTable(this);
+        //GUI-Update
+        System.out.println(Name + " Left the Restaurant");
         Button.setText(String.valueOf(TableID));
         Button.setBackground(Color.GRAY);
-
-        System.out.println(Name + " Left the Restaurant");
-
     }
 }
